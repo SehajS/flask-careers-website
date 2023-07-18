07095313bd9f897a10ab092e72344d3707ddb8ef
SehajS <singhsp7977@gmail.com>
2023-07-18 05:26:36 +0000

    added a dynamic data onto html templates

--- app.py
+++ app.py
@@ -2,10 +2,31 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS = [
+  {
+    'id': 1,
+    'title': 'Data Analyst',
+    'location': 'Vancouver, BC',
+    'salary': '90,000 CAD'
+  },
+  {
+    'id': 2,
+    'title': 'Data Scientist',
+    'location': 'Vancouver, BC',
+    # 'salary': '110,000 CAD'
+  },
+  {
+    'id': 3,
+    'title': 'Frontend Developer',
+    'location': 'Winnipeg, MB',
+    'salary': '75,000 CAD'
+  }
+]
 
 @app.route("/")
 def hello_world():
-  return render_template('home.html')
+  return render_template('home.html',
+                         jobs=JOBS)
 
 
 if __name__ == "__main__":
