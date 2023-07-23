bba12724da872a784c7cf1ff475695e425a1c142
SehajS <singhsp7977@gmail.com>
2023-07-23 08:19:19 +0000

    application form db functionality added

--- database.py
+++ database.py
@@ -29,3 +29,10 @@ def load_job_from_db(id):
     if len(rows) == 0:
       return None
     return rows[0]._asdict()
+
+
+def add_application_to_db(job_id, data):
+  with engine.connect() as conn:
+    query = text(f"""INSERT INTO applications (job_id, full_name, email, linkedin, education, resume_url, experience)
+    VALUES ({job_id}, "{data['full_name']}", "{data['email']}", "{data['linkedin']}", "{data['education']}", "{data['resume_url']}", "{data['experience']}")""")
+    conn.execute(query)
